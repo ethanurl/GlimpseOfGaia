b3c15fb50bf10fdde9f5071b41df7fe693198174
Samip Niraula <samiprichardniraula@gmail.com>
2024-03-13 15:42:31 -0700

    Fixing build settings

--- Assets/Scripts/Game/PlayerScript.cs
+++ Assets/Scripts/Game/PlayerScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
